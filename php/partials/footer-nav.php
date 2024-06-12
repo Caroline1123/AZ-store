0e6e9b7b34f66702a8fbc5e0342925c4f2f38ba6
Caroline <deconinck.caro@gmail.com>
2024-06-12 19:10:18 +0200

    added add to cart remove from cart functions

--- php/partials/footer-nav.php
+++ php/partials/footer-nav.php
@@ -1,10 +1,11 @@
-<div class="container-fluid">    
-    <div class="navbar navbar-dark justify-content-center position-absolute bottom-0 start-50 translate-middle" id="navbarNavAltFooter">
-      <div class="navbar-nav">
-        <a class="nav-link active px-2" aria-current="page" href="#">Home</a>
-        <a class="nav-link px-2" href="#">About</a>
-        <a class="nav-link px-2" href="#">Products</a>
-        <a class="nav-link px-2" href="#">Content</a>
-      </div>
+<div class="container-fluid">
+  <div class="navbar navbar-dark justify-content-center position-absolute bottom-0 start-50 translate-middle"
+    id="navbarNavAltFooter">
+    <div class="navbar-nav">
+      <a class="nav-link active px-2" aria-current="page" href="#">Home</a>
+      <a class="nav-link px-2" href="#">About</a>
+      <a class="nav-link px-2" href="#">Products</a>
+      <a class="nav-link px-2" href="#">Content</a>
     </div>
   </div>
+</div>
--- php/shopping-cart.php
+++ php/shopping-cart.php
@@ -9,6 +9,8 @@ if (session_status() != PHP_SESSION_ACTIVE) {
     ];
 }
 
+// Adds one item to the cart
+//Parameter : int :  product ID (must match data.json product ID)
 function add_item($product_id)
 {
     $product_index = array_search($product_id, $_SESSION["cart"]["product_id"]);
@@ -19,6 +21,9 @@ function add_item($product_id)
         $_SESSION["cart"]["quantity"][$product_index] += 1;
     }
 }
+
+// Removes one item from the cart. 
+//Parameter : int :  product ID (must match data.json product ID)
 function remove_item($product_id)
 {
     $product_index = array_search($product_id, $_SESSION["cart"]["product_id"]);
@@ -41,29 +46,38 @@ add_item(1);
 add_item(2);
 add_item(1);
 remove_item(1);
-
-for ($i = 0; $i < count($_SESSION["cart"]["product_id"]); $i++) {
-    echo "" . $_SESSION["cart"]["product_id"][$i] . ":" . $_SESSION["cart"]["quantity"][$i] . "<br>";
-}
-
-// Example of JSON object for product
-//{
-//     "id": 1,
-//     "product": "Nike Air Max 270",
-//     "price": 140,
-//     "image_url": "/assets/img/nike-air-max-279.jpeg"
-//   }
-
-// Cart should have : id of product, quantity
+add_item(25);
 
 // Shopping cart page should show : product img, product name, product price and quantity
-$SESSION["cart"] = [
-    // "products" => [],
-];
 
-if (count($_SESSION['cart']['product_id']) == 0) {
-    echo "no items in the cart";
+if (count($_SESSION['cart']['product_id']) === 0) {
+    "no item found in the cart";
 }
+
+
 ?>
 
-<?php require 'partials/nav.php' ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AZ-store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/8a245e3c89.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="./../assets/css/styles.css">
+
+<body>
+    <?php require 'partials/nav.php' ?>
+    <?php require 'partials/footer-nav.php' ?>
+
+</body>
