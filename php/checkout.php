1db8b46fb47f0c135dc6b6822a923ed5df672988
Caroline <deconinck.caro@gmail.com>
2024-06-13 14:50:55 +0200

    removed footer: display issue

--- php/checkout.php
+++ php/checkout.php
@@ -96,5 +96,5 @@ require_once ("./cart-functions.php")
         </div>
         <?php echo display_summary() ?>
     </div>
-    <?php require 'partials/footer-nav.php' ?>
+    <!-- <?php require 'partials/footer-nav.php' ?> -->
 </body>
