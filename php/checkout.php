ee620adb47e8c3fb19cfc5067a91e01c97f633dd
Caroline <deconinck.caro@gmail.com>
2024-06-13 12:12:24 +0200

    fixed order summary layout

---
+++ php/checkout.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once ("./cart-functions.php")
+
+    // Need to disable checkout button if page is checkout.php
+
+
+    ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AZ-store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <script defer src="./../assets/js/script.js"></script>
+    <script src="https://kit.fontawesome.com/8a245e3c89.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="./../assets/css/styles.css">
+
+<body>
+    <?php require 'partials/nav.php' ?>
+    <h3 class="m-3 text-center">Your Details</h3>
+    <div class="shopping-cart border rounded d-flex m-3 p-2">
+        <div class="overview d-flex flex-column">
+
+        </div>
+        <?php echo display_summary() ?>
+    </div>
+    <?php require 'partials/footer-nav.php' ?>
+</body>
